b370df61abe32246df352aa025fb82a87efd9bbb
Luiz-Hen-Reis <lhcontato2020@gmail.com>
2022-12-22 15:17:26 -0300

    testing the app

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -19,19 +19,50 @@ describe("Home", () => {
     cy.scrollTo("bottom");
 
     cy.get("div").contains("Load More").click();
-    cy.wait(3000);
     cy.scrollTo("bottom");
-
     cy.get("a").contains("venonat #48");
   });
 
-  it('should find the search input and type a pokemon name, submit the form and show the pokemon on screen', () => {
-    const pokemon = 'charmander';
+  it("should find the search input and type a pokemon name, submit the form and show the pokemon on screen", () => {
+    const pokemon = "ivysaur";
 
-    cy.get('.Header_input__CtHVr').type(pokemon);
-    cy.get('.Header_button__4rS9_').click();
+    cy.get(".Header_input__CtHVr").type(pokemon);
+    cy.get(".Header_button__4rS9_").click();
+
+    cy.url().should("contain", "search?p=ivysaur");
     cy.wait(3000);
-    cy.get("a").contains("charmander");
+    cy.get("a").contains("ivysaur").should('be.visible');
+  });
+
+  it("should find the search input and type a pokemon name, submit the form and show the pokemon on screen, click on the card and go to pokemon info page", () => {
+    const pokemon = "mew two";
+
+    cy.get(".Header_input__CtHVr").type(pokemon);
+    cy.get(".Header_button__4rS9_").click();
+
+    cy.url().should("contain", "search?p=mew+two");
+    cy.wait(3000);
+    cy.get("a").contains("mewtwo").click();
+
+    cy.url().should("contain", "/pokemon/mewtwo");
+    cy.get(".PokemonInfo_pokemonInfo__tgRu8").contains("Mewtwo");
+  });
+
+  it("should find the specified pokemon, click on the card and go to the pokemon info page", () => {
+    cy.get("a").contains("butterfree #12").click();
+
+    cy.url().should("contain", "/pokemon/butterfree");
+    cy.get(".PokemonInfo_pokemonInfo__tgRu8").contains("Butterfree");
+  });
+
+  it('should find the specified type button, click on it, verify if showed the pokemons that has that type, click on one and go to pokemon info page', () => {
+    cy.get('a').contains('dark').click();
+
+    cy.url().should('contain', '/type/dark');
+    cy.get('a').contains('houndour #228').click();
+
+    cy.url().should("contain", "/pokemon/houndour");
+    cy.get(".PokemonInfo_pokemonInfo__tgRu8").contains("Houndour");
   })
 });
 
